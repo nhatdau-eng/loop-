ea8bdab753225bf7c663d1310a78b4ec04a5201c
nhatdau-eng <nhat.dau@student.codegym.vn>
2026-07-10 08:56:54 +0700

    hien thi cac loai hinh

---
+++ Main/Main.java
@@ -0,0 +1,88 @@
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int choice;
+        do {
+            System.out.println("MENU");
+            System.out.println("1. Print the rectangle");
+            System.out.println("2. Print the square triangle");
+            System.out.println("3. Print isosceles triangle");
+            System.out.println("4. Exit");
+            System.out.print("Enter your choice: ");
+
+            choice = sc.nextInt();
+
+            switch (choice) {
+                case 1:
+                    for (int i = 1; i <= 3; i++) {
+                        for (int j = 1; j <= 7; j++) {
+                            System.out.print("* ");
+                        }
+                        System.out.println();
+                    }
+                    break;
+                case 2:
+                    System.out.println("Bottom-left:");
+                    for (int i = 1; i <= 5; i++) {
+                        for (int j = 1; j <= i; j++) {
+                            System.out.print("* ");
+                        }
+                        System.out.println();
+                    }
+                    System.out.println("\nTop-left:");
+                    for (int i = 5; i >= 1; i--) {
+                        for (int j = 1; j <= i; j++) {
+                            System.out.print("* ");
+                        }
+                        System.out.println();
+                    }
+                    System.out.println("\nBottom-right:");
+                    for (int i = 1; i <= 5; i++) {
+                        for (int j = 1; j <= 5 - i; j++) {
+                            System.out.print("  ");
+                        }
+                        for (int j = 1; j <= i; j++) {
+                            System.out.print("* ");
+                        }
+                        System.out.println();
+                    }
+                    System.out.println("\nTop-right:");
+                    for (int i = 5; i >= 1; i--) {
+                        for (int j = 1; j <= 5 - i; j++) {
+                            System.out.print("  ");
+                        }
+                        for (int j = 1; j <= i; j++) {
+                            System.out.print("* ");
+                        }
+                        System.out.println();
+                    }
+                    break;
+                case 3:
+                    int h = 5;
+                    for (int i = 1; i <= h; i++) {
+                        for (int j = 1; j <= h - i; j++) {
+                            System.out.print(" ");
+                        }
+                        for (int j = 1; j <= 2 * i - 1; j++) {
+                            System.out.print("*");
+                        }
+                        System.out.println();
+                    }
+                    break;
+
+                case 4:
+                    System.out.println("Exit!");
+                    break;
+
+                default:
+                    System.out.println("Invalid choice!");
+            }
+
+            System.out.println();
+
+        } while (choice != 4);
+
+        sc.close();
+    }
+}
